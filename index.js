4edcc4abe681d680a2565af36537afa40753280f
snehamoybag <snehamoybag>
2023-09-10 01:33:52 +0530

    add form validation
    show error stylings when user submits wrong input

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const inputEls = document.querySelectorAll(".form__input");
-const passwordEl = document.querySelector("password");
-const confirmPasswordsEl = document.querySelector("#confirm-password");
+const passwordEl = document.querySelector("#password");
+const confirmPasswordEl = document.querySelector("#confirm-password");
+const passErrorMsgEl = document.querySelector("#error-pass-msg");
 
-inputEls.forEach((inputEl) =>
-  inputEl.addEventListener("invalid", () => {
-    inputEl.classList.add("error");
-  })
-);
+// manually add and remove invalid error
+const throwPassMismatchError = function () {
+  passwordEl.classList.add("error");
+  confirmPasswordEl.classList.add("error");
+  passErrorMsgEl.classList.remove("hidden");
+  confirmPasswordEl.focus(); //switch focus
+};
+const removePassMismatchError = function () {
+  confirmPasswordEl.classList.remove("error");
+  passwordEl.classList.remove("error");
+  passErrorMsgEl.classList.add("hidden");
+};
+
+// add invalid error on all input field
+// checks HTML pattern for validity
+inputEls.forEach((inputEl) => {
+  inputEl.addEventListener("invalid", () => inputEl.classList.add("error"));
+});
+
+// check for password mismatch while typying in confirm field
+confirmPasswordEl.addEventListener("input", () => {
+  passwordEl.value !== confirmPasswordEl.value
+    ? throwPassMismatchError()
+    : removePassMismatchError();
+});
+
+// check for password match on form submit
+const formEl = document.querySelector("#form");
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (passwordEl.value !== confirmPasswordEl.value) {
+    throwPassMismatchError();
+  } else {
+    removePassMismatchError();
+    alert("Sign up successful! Thanks for checking out this project! :)");
+  }
+});
